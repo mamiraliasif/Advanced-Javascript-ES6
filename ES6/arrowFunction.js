72048bab39587d74725ce11d17a5ba399b5493a6
Muhammad Amir Ali Asif <152416865+mamiraliasif@users.noreply.github.com>
2024-09-22 21:52:28 +0500

    Add files via upload

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,21 @@
+
+
+let sum = (x,y) => {
+    return x + y ;
+}
+
+console.log(sum(5,7)); // 12
+
+
+let exp = (x,y ) => {
+    return x**y ;
+}
+
+console.log(exp(2,10)); 
+
+
+let getName = (name) =>{
+    return name;
+}
+
+console.log(getName("AMIR"));
